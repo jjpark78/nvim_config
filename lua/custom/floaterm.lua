6235693945b096a726cd36000cdb20ed52e5ea91
jaejin park <jjpark78@outlook.com>
2022-05-21 13:40:20 +0900

    change option

--- lua/custom/floaterm.lua
+++ lua/custom/floaterm.lua
@@ -1,5 +1,5 @@
 vim.g.floaterm_wintype = 'float'
 vim.g.floaterm_position = 'bottom'
-vim.g.floaterm_width = 0.98 
-vim.g.floaterm_height = 0.5 
+vim.g.floaterm_width = 0.99 
+vim.g.floaterm_height = 0.5
 
--- lua/custom/lazygit.lua
+++ lua/custom/lazygit.lua
@@ -11,3 +11,4 @@ vim.g.lazygit_floating_window_winblend = 0 -- transparency of floating window
 vim.g.lazygit_floating_window_scaling_factor = 0.9 -- scaling factor for floating window
 vim.g.lazygit_floating_window_use_plenary = 0 -- use plenary.nvim to manage floating window if available
 vim.g.lazygit_use_neovim_remote = 1 -- fallback to 0 if neovim-remote is not installed
+
